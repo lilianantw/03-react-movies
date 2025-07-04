cc66a6eda22a0941f029733d687d3de770729e1c
liliana_toiunda <liliana.ntw@gmail.com>
2025-07-04 17:39:49 +0300

    add file

---
+++ src/components/App/App.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import toast, { Toaster } from "react-hot-toast";
+
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieModal from "../MovieModal/MovieModal";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+
+const App = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async (query: string) => {
+    setMovies([]);
+    setError(false);
+    setIsLoading(true);
+
+    try {
+      const results = await fetchMovies(query);
+
+      if (results.length === 0) {
+        toast("No movies found for your request.");
+      }
+
+      setMovies(results);
+    } catch {
+      setError(true);
+      toast.error("Failed to fetch movies. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSelectMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+  };
+
+  return (
+    <div className={styles.app}>
+      <Toaster position="top-right" />
+      <SearchBar onSubmit={handleSearch} />
+
+      {isLoading && <Loader />}
+      {error && <ErrorMessage />}
+      {!isLoading && !error && movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+      )}
+
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
+      )}
+    </div>
+  );
+};
+
+export default App;
